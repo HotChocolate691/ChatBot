2d1c669ebd887a70e97ca94e5d1a59e7b47e43e1
HotChocolate691 <devsharmaa04@gmail.com>
2025-02-27 21:32:19 +0530

    updated version 27 feb 2025

--- src/api/OpenAI.js
+++ src/api/OpenAI.js
@@ -1,31 +1,23 @@
 import axios from "axios";
 
-const API_URL =
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=GEMINI_API_KEY";
 const API_KEY = "AIzaSyC3KjI8EXR0S152ocFcD0ffskNED5SJmEE";
 
 export const getResponse = async (userMessage) => {
-  
   try {
-    console.log("Sending request with prompt:", userMessage);
-
-    const res = await axios({
-      url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
-      method: "POST",
-      data: {
+    const res = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      {
         contents: [{ parts: [{ text: userMessage }] }],
-      },
-    });
-    return res.data.candidates[0].content.parts[0].text;
+      }
+    );
+
+    let ans = res.data.candidates?.[0]?.content?.parts?.[0]?.text;
+
+    console.log(ans)
+    return ans || "No response";
 
   } catch (error) {
-    if (error.response) {
-      console.error("Error Response:", error.response.data);
-    } else if (error.request) {
-      console.error("No Response Received:", error.request);
-    } else {
-      console.error("Error Message:", error.message);
-    }
+    console.error("API Error:", error);
     return "Sorry, something went wrong. Please try again.";
   }
 };
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,20 +1,26 @@
 import { FaRegFaceMehBlank } from "react-icons/fa6";
 import { SiApifox } from "react-icons/si";
+import ReactMarkdown from 'react-markdown';
+import "../App.css";
 
 const Message = ({ text, isBot }) => (
   <div
-    className={`flex pt-1 md:pt-2 mx-4 my-3 rounded-lg bg-gray-200 ${
+    className={`flex pt-2 px-4 py-2 mx-4 my-3 rounded-lg ${
       isBot
-        ? "bg-transparent text-purple-600  self-start"
-        : "bg-transparent  text-pink-400 font-semibold self-end"
+        ? "bg-transparent text-purple-600 self-start"
+        : "bg-transparent text-pink-400 font-semibold self-end"
     }`}
   >
-    <div className="mr-3 mt-1">
-      {isBot
-        ? <SiApifox className="text-purple-500 text-lg" />
-        : <FaRegFaceMehBlank className="text-pink-500 text-lg" /> }
-    </div><span>{ text }</span>
-    
+    <div className="mr-3 mt-2">
+      {isBot ? (
+        <SiApifox className="text-purple-500 text-lg" />
+      ) : (
+        <FaRegFaceMehBlank className="text-pink-500 text-lg" />
+      )}
+    </div>
+    <div className="result">
+      <ReactMarkdown>{text}</ReactMarkdown>
+    </div>
   </div>
 );
 
